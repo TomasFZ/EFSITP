5c1c18e9a29bcbec73f1fee15c7f972b3193756c
TomasFZ <tomyfernandezabala@gmail.com>
2024-10-18 18:05:53 -0300

    jdzn

--- efsifront/src/components/EventDetails.jsx
+++ efsifront/src/components/EventDetails.jsx
@@ -4,19 +4,16 @@ import axios from 'axios';
 import { useAuth } from './AuthContext';
 
 function EventDetails() {
-  const { id } = useParams(); // Obtener el ID del evento desde la URL
+  const { id } = useParams(); // saca el ID del evento desde la url
   const [eventDetails, setEventDetails] = useState(null);
-  // eslint-disable-next-line
-  const { token, userId, user } = useAuth(); // Usar el hook useAuth para obtener token y userId
+  const { token, userId, user } = useAuth(); // useAuth token y userId
   console.log("Token AUTH:", token);
   console.log("UserId AUTH:", userId);
-  // eslint-disable-next-line
   const [locations, setLocations] = useState({});
-  // eslint-disable-next-line
-  const [enrollments, setEnrollments] = useState({});
+  //const [enrollments, setEnrollments] = useState({});
 
   useEffect(() => {
-    // Fetch para obtener los detalles del evento
+    // Fetch detalles evento
     const fetchEventDetails = async () => {
       try {
         const response = await axios.get(`http://localhost:3001/api/event/${id}`, {
@@ -31,40 +28,36 @@ function EventDetails() {
     };
 
     fetchEventDetails();
-  }, [id, token]);
+  }, [id, token]); //si el token expira se re-renderiza (lo mismo el id del event)
 
   const handleSubscribe = async () => {
     try {
-      // Fetch de event-location para obtener el número de locations disponibles
+      // Fetch event-location numero de locations disponibles
       const locationResponse = await axios.get(`http://localhost:3001/api/event-location/${eventDetails.id_event_location}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
-      const totalLocations = locationResponse.data.length;  
-      setLocations(prev => ({ ...prev, [eventDetails.id_event_location]: totalLocations }));
+      const totalLocations = locationResponse.data.length;   //num locations disponibles
+      setLocations(prev => ({ ...prev, [eventDetails.id_event_location]: totalLocations })); //???
   
-      // Fetch de event-enrollment para obtener los usuarios suscritos
+      // Fetch event-enrollment para los usuarios suscritos
       const enrollmentResponse = await axios.get(`http://localhost:3001/api/event/${eventDetails.event_id}/enrollment`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
   
-      // Verificar si el usuario ya está inscrito
-      // eslint-disable-next-line
+     
       const isAlreadyEnrolled = enrollmentResponse.data.collection.some(user => user.username === user.username);
       
       if (isAlreadyEnrolled) {
         alert('Ya estás suscrito a este evento.');
-        return; // Detener el proceso si ya está inscrito
+        return; 
       }
   
-      const enrolledUsers = enrollmentResponse.data.collection.length;
-      
-      // Verificar si hay espacios disponibles
-      if (enrolledUsers < totalLocations) { // Usar < para permitir la suscripción
-        // eslint-disable-next-line
+      const enrolledUsers = enrollmentResponse.data.collection.length; //num de personas suscritas
+      if (enrolledUsers < totalLocations) { 
         const subscribeResponse = await axios.post(`http://localhost:3001/api/event/${eventDetails.event_id}/enrollment`, {
           userId: userId, // Consigue el userId desde el authContext
         }, {
@@ -72,9 +65,9 @@ function EventDetails() {
             Authorization: `Bearer ${token}`,
           },
         });
-        alert(`Te has suscrito al evento con ID: ${eventDetails.id}`);
+        alert(`Te has suscrito al event`);
       } else {
-        alert('No hay más cupos disponibles para este evento.');
+        alert('No hay mas cupos disponibles para este evento.');
       }
     } catch (error) {
       console.error('Error durante el proceso de suscripción:', error);
--- efsifront/src/components/EventsList.jsx
+++ efsifront/src/components/EventsList.jsx
@@ -1,88 +1,4 @@
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
 
-// function EventsList() {
-//   const [events, setEvents] = useState([]);
-//   const [locations, setLocations] = useState({});
-//   const [enrollments, setEnrollments] = useState({});
-//   const token = localStorage.getItem('token');
-
-//   useEffect(() => {
-//     // Fetch inicial para cargar los eventos
-//     const fetchEvents = async () => {
-//       try {
-//         const response = await axios.get('http://localhost:3001/api/event', {
-//           headers: {
-//             Authorization: `Bearer ${token}`,
-//           },
-//         });
-//         setEvents(response.data.collection);
-//       } catch (error) {
-//         console.error("Error fetching events:", error);
-//       }
-//     };
-
-//     fetchEvents();
-//   }, [token]);
-
-//   // Función para suscribirse a un evento
-//   const handleSubscribe = async (eventId) => {
-//     try {
-//       // Fetch de event-location para obtener el número de locations disponibles
-//       console.log("Header Sub")
-//       const locationResponse = await axios.get(`http://localhost:3001/api/event-location/${eventId}`, {
-//         headers: {
-//           Authorization: `Bearer ${token}`,
-//         },
-//       });
-//       console.log("Header Sub Pass")
-//       const totalLocations = locationResponse.data.length;
-//       setLocations(prev => ({ ...prev, [eventId]: totalLocations }));
-
-//       // Fetch de event-enrollment para obtener los usuarios suscritos
-//       console.log("Header Enroll")
-//       const enrollmentResponse = await axios.get(`http://localhost:3001/api/${eventId}/enrollment`, {
-//         headers: {
-//           Authorization: `Bearer ${token}`,
-//         },
-//       });
-//       console.log("Header Enroll Pass")
-//       const enrolledUsers = enrollmentResponse.data.users.length;
-//       setEnrollments(prev => ({ ...prev, [eventId]: enrolledUsers }));
-
-//       // Verificar si hay espacios disponibles
-//       if (enrolledUsers < totalLocations) {
-//         alert(`Te has suscrito al evento con ID: ${eventId}`);
-//         // Aquí iría el código para realizar la suscripción, si procede
-//       } else {
-//         alert('No hay más cupos disponibles para este evento.');
-//       }
-//     } catch (error) {
-//       console.error('Error during subscription process:', error);
-//     }
-//   };
-
-//   return (
-//     <div className="events-list">
-//       <h2>Eventos Disponibles</h2>
-//       <ul>
-//         {events.map(event => (
-//           <li key={event.id}>
-//             <span>{event.name}</span>
-//             <button onClick={() => handleSubscribe(event.id)}>Suscribirse</button>
-//             {locations[event.id] !== undefined && enrollments[event.id] !== undefined && (
-//               <div>
-//                 <p>{`Lugares disponibles: ${locations[event.id] - enrollments[event.id]}`}</p>
-//               </div>
-//             )}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default EventsList;
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -101,7 +17,7 @@ function EventsList() {
     max_assistance: ""
   });
   const token = localStorage.getItem('token');
-  const userId = localStorage.getItem('userId');
+  const userId = localStorage.getItem('userId'); //cambiar a que usen AuthContext. 
   const navigate = useNavigate();
 
   const fetchEvents = async () => {
@@ -119,7 +35,6 @@ function EventsList() {
 
   useEffect(() => {
     fetchEvents();
-    // eslint-disable-next-line
   }, [token]);
 
   const handleEventClick = (event) => {
@@ -155,7 +70,7 @@ function EventsList() {
 
       console.log('Event created:', response.data);
       
-      // Fetch events again to ensure we have the most up-to-date list
+      // Fetch pq sino no aparece el recien creado hasta despues de recargar la pagina
       await fetchEvents();
       
       setNewEvent({
